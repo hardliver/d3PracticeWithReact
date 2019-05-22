364e122c0333d9cad699a89608014b40d9da6671
hammerandnail <neil.ye@gooee.com>
2019-05-22 17:18:54 +0800

    Basic SVG shapes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,28 @@ import { select } from 'd3-selection';
 class App extends Component {
 
   componentDidMount() {
-    select('.d3').append('p').text('Hi, what\'s up');
+    let canvas = select('.d3')
+      .append('svg')
+      .attr('width', 500)
+      .attr('height', 500)
+
+    let circle = canvas.append('circle')
+      .attr('cx', 250)
+      .attr('cy', 250)
+      .attr('r', 50)
+      .attr('fill', 'red');
+
+    let rect = canvas.append('rect')
+      .attr('width', 100)
+      .attr('height', 50);
+
+    let line = canvas.append('line')
+      .attr('x1', 0)
+      .attr('y1', 100)
+      .attr('x2', 400)
+      .attr('y2', 400)
+      .attr('stroke', 'green')
+      .attr('stroke-width', 10);
   }
 
   render() {
