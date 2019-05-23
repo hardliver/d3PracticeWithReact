e41cc2462bddfeb479d510e143332f55c30a6b08
hammerandnail <neil.ye@gooee.com>
2019-05-23 16:59:55 +0800

    Paths

--- src/App.js
+++ src/App.js
@@ -3,42 +3,35 @@ import './App.css';
 import * as d3 from "d3";
 import { select } from 'd3-selection';
 
-import data from './data/data.json'
-
 class App extends Component {
-
-  state = {
-    people: []
-  }
-
-  componentWillMount() {
-    this.setState({
-      people: data
-    })
-  }
 
   componentDidMount() {
     let canvas = select('.d3')
       .append('svg')
       .attr('width', 500)
-      .attr('height', 500)
+      .attr('height', 500);
 
-    canvas.selectAll('rect')
-      .data(this.state.people)
-      .enter()
-        .append('rect')
-        .attr('width', (d) => { return d.age * 10; })
-        .attr('height', 48)
-        .attr('y', (d, i) => { return i * 50; })
-        .attr('fill', 'blue');
+    let data = [
+      {x: 10, y: 20},
+      {x: 40, y: 60},
+      {x: 50, y: 70}
+    ];
 
-    canvas.selectAll('text')
-      .data(this.state.people)
+    let group = canvas.append('g')
+      .attr('transform', "translate(100, 100)")
+
+    let line = d3.line()
+      .x((d) => { return d.x; })
+      .y((d) => { return d.y; });
+
+    group.selectAll('path')
+      .data([data])
       .enter()
-        .append('text')
-        .attr('fill', 'white')
-        .attr('y', (d, i) => { return i * 50 + 24; })
-        .text((d) => { return d.name })
+        .append('path')
+        .attr('d', line)
+        .attr('fill', 'none')
+        .attr('stroke', '#000')
+        .attr('stroke-width', 10);
   }
 
   render() {
